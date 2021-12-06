9c10058c9ea607b6a97a38552313b083528a7eaf
EderReisS <ederdt.3@gmail.com>
2021-12-05 23:28:03 -0300

    ok! no host local #1

--- src/components/Conversor.js
+++ src/components/Conversor.js
@@ -25,6 +25,7 @@ export default class Conversor extends Component {
 
         this.converter = this.converter.bind(this);
         this.calcular_hipotenusa = this.calcular_hipotenusa.bind(this)
+        this.calcular_cateto = this.calcular_cateto.bind(this)
      }
 
      converter(){
@@ -44,11 +45,11 @@ export default class Conversor extends Component {
     }
     calcular_hipotenusa(){
         
-        let metodo = "calcular_hipotenusa";
+        let metodo = `${this.state.metodo_1}`;
 
-        let var1 = `${this.props.lado1_h}`;
+        let var1 = `${this.state.lado1_h}`;
 
-        let var2 = `${this.props.lado2_h}`;
+        let var2 = `${this.state.lado2_h}`;
 
 
         let url = `https://api-pitagoras-ederreis.herokuapp.com/${metodo}/${var1}/${var2}`;
@@ -69,40 +70,73 @@ export default class Conversor extends Component {
                 this.setState({hip_h})
             });
     }
+    calcular_cateto(){
+        
+        let metodo = `${this.state.metodo_2}`;
+
+        let var1 = `${this.state.lado1_c}`;
+
+        let var2 = `${this.state.lado2_c}`;
+
+
+        let url = `https://api-pitagoras-ederreis.herokuapp.com/${metodo}/${var1}/${var2}`;
+
+        fetch(url)
+            .then( res => {
+                return res.json()
+            })
+            .then(json => {
+                let cat_menor_c = json["cateto_menor"];
+
+                let cat_maior_c = json["cateto_maior"];
+
+                let hip_c = json["hipotenusa"];
+
+                this.setState({cat_menor_c})
+                this.setState({cat_maior_c})
+                this.setState({hip_c})
+            });
+    }
 
 
     render() {
         return (
             <div className="conversor">
-                <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
+                <h1>Método para calculo da hipotenusa</h1>
                 <li>
                     <p>Lado 1</p>
                     <p>
-                        <input className="input" onChange={(event) => { this.setState({moedaA_valor:event.target.value})}} type="text"></input>
-                        <input className="button" type="button" value="ok" onClick={this.converter}></input>
-                    </p>
-                </li>
-                <h2>valor convertido: <span> {this.state.moedaB_valor} </span></h2>
-                
-                <h2>Método para calculo da hipotenusa</h2>
-                <li>
-                    <p>Lado 1</p>
-                    <p>
-                        <input className="input" onChange={(event) => { this.setState({lado1_h:event.target.value})}} type="text"></input>
+                        <input className="input" onChange={(event) => { this.setState({lado1_h:event.target.value})}} type="number"></input>
                     </p>                    
                 </li>
                 <li>
                     <p>Lado 2</p>
                     <p>
-                        <input className="input" onChange={(event) => { this.setState({lado2_h:event.target.value})}} type="text"></input>
+                        <input className="input" onChange={(event) => { this.setState({lado2_h:event.target.value})}} type="number"></input>
                     </p>                    
                 </li>
                 <input className="button" type="button" value="ok" onClick={this.calcular_hipotenusa}></input>
                 <h2>Cateto maior, a = <span> {this.state.cat_maior_h} </span></h2>
                 <h2>Cateto menor, b = <span> {this.state.cat_menor_h} </span></h2>
                 <h2>Hipotenusa, c = <span> {this.state.hip_h} </span></h2>
-                
 
+                <h1>Método para calculo do cateto</h1>
+                <li>
+                    <p>Lado 1</p>
+                    <p>
+                        <input className="input" onChange={(event) => { this.setState({lado1_c:event.target.value})}} type="number"></input>
+                    </p>                    
+                </li>
+                <li>
+                    <p>Lado 2</p>
+                    <p>
+                        <input className="input" onChange={(event) => { this.setState({lado2_c:event.target.value})}} type="number"></input>
+                    </p>                    
+                </li>
+                <input className="button" type="button" value="ok" onClick={this.calcular_cateto}></input>
+                <h2>Cateto maior, a = <span> {this.state.cat_maior_c} </span></h2>
+                <h2>Cateto menor, b = <span> {this.state.cat_menor_c} </span></h2>
+                <h2>Hipotenusa, c = <span> {this.state.hip_c} </span></h2>
             </div>
         )
     }
